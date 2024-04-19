707d8b3ae3579a79de7606096224d2ae9c55293e
AnastasiiaTkachuk <anastaisha.580@gmail.com>
2024-04-19 11:52:37 +0200

    Add Lombok Logging

--- src/main/java/anastasiia/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/anastasiia/springframework/spring6restmvc/controller/BeerController.java
@@ -1,11 +1,23 @@
 package anastasiia.springframework.spring6restmvc.controller;
 
+import anastasiia.springframework.spring6restmvc.model.Beer;
 import anastasiia.springframework.spring6restmvc.service.BeerService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 
+import java.util.UUID;
+
+@Slf4j
 @AllArgsConstructor
 @Controller
 public class BeerController {
     private final BeerService beerService;
+
+    public Beer getBeerId(UUID id){
+
+        log.debug("Get Beer by Id - in controller");
+
+        return beerService.getBeerById(id);
+    }
 }
