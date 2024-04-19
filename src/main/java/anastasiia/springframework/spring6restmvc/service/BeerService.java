2693b5c53afdf5fbc369c5c875c155d69b2f1aae
AnastasiiaTkachuk <anastaisha.580@gmail.com>
2024-04-19 11:30:34 +0200

    Add Lombok Builder

---
+++ src/main/java/anastasiia/springframework/spring6restmvc/service/BeerService.java
@@ -0,0 +1,8 @@
+package anastasiia.springframework.spring6restmvc.service;
+
+import anastasiia.springframework.spring6restmvc.model.Beer;
+
+public interface BeerService {
+
+    Beer getBeerById();
+}
---
+++ src/main/java/anastasiia/springframework/spring6restmvc/service/BeerServiceImpl.java
@@ -0,0 +1,25 @@
+package anastasiia.springframework.spring6restmvc.service;
+
+import anastasiia.springframework.spring6restmvc.model.Beer;
+import anastasiia.springframework.spring6restmvc.model.BeerStyle;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class BeerServiceImpl implements BeerService {
+    @Override
+    public Beer getBeerById() {
+        return Beer.builder()
+                .id(UUID.randomUUID())
+                .version(1)
+                .beerName("Galaxy Cat")
+                .beerStyle(BeerStyle.PALE_ALE)
+                .upc("12345")
+                .price(new BigDecimal("12.99"))
+                .quantityOnHand(122)
+                .createdDate(LocalDateTime.now())
+                .updateDate(LocalDateTime.now())
+                .build();
+    }
+}
